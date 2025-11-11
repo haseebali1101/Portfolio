ab278e4a5c75cbf0fc4970703d9b9182fcf77dfa
haseeb1101 <haseeb1102@gmail.com>
2025-11-10 23:12:40 -0500

    Load campus background image via PUBLIC_URL

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const campusBackground = `${process.env.PUBLIC_URL}/campus-bg.jpg`;
+if (campusBackground) {
+  document.body.style.setProperty('--app-background-image', `url("${campusBackground}")`);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
